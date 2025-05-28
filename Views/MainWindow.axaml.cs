35ce984fc931dbcd22fa257a349f870f1d88a09e
Zain Farhan <zainfarhan.work@gmail.com>
2025-05-28 16:33:46 +0500

    initial commit lmao

---
+++ Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Chess_Prototype.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
